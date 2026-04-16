23764d19c479969bee3579f3e7d73314995114e3
avery macasa <averymikasa@gmail.com>
2026-04-16 16:49:17 +0800

    new icon

--- metro.config.cjs
+++ metro.config.cjs
@@ -2,6 +2,7 @@ const { getDefaultConfig } = require('expo/metro-config')
 
 /** @type {import('expo/metro-config').MetroConfig} */
 const config = getDefaultConfig(__dirname)
+config.resolver.assetExts.push('jfif')
 
 const { withTamagui } = require('@tamagui/metro-plugin')
 module.exports = withTamagui(config, {
