337b4713c6463da465f5780096fa27267af92454
Eybri <averymikasa@gmail.com>
2026-05-10 21:01:47 +0800

    feat: implement onboarding startup screen and brand intro view with navigation logic

--- src/screens/onboarding/brand-intro/BrandIntroView.tsx
+++ src/screens/onboarding/brand-intro/BrandIntroView.tsx
@@ -34,7 +34,7 @@ export const BrandIntroView: React.FC<BrandIntroViewProps> = ({ vm }) => {
                         alignItems: 'center',
                     }}>
                         <RNImage
-                            source={require('../../../../assets/images/logo2-nobg.png')}
+                            source={require('../../../../assets/images/icon-blue.png')}
                             style={{ width: width * 0.38, height: width * 0.38 }}
                             resizeMode="contain"
                         />
